bb27f070692e0150839bf0d544cc14bd7b32972c
Windolon <windyscapexe@gmail.com>
2026-01-06 10:36:23 +0800

    fish: forgot to actually choose the default theme

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     fish_add_path $HOME/.local/bin $HOME/bin
     set -gx EDITOR nvim
+    fish_config theme choose "fish default"
 
     set -gx pure_color_success green
 end
