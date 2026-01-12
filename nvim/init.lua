c7657de102c02b4266ca794114706397b0103464
Windolon <windyscapexe@gmail.com>
2026-01-12 11:36:06 +0800

    nvim: autosession everywhere

--- nvim/init.lua
+++ nvim/init.lua
@@ -314,9 +314,7 @@ config_fzf()
 config_git()
 
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-require("auto-session").setup({
-    suppressed_dirs = { "~/", "~/Downloads/", "/" },
-})
+require("auto-session").setup()
 
 require("guess-indent").setup({})
 
