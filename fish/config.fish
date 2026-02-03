3bc12c6ec5c3f5ef707d5d4561d5a5a5cb7f3e5f
Windolon <windyscapexe@gmail.com>
2026-02-03 10:06:59 +0800

    general: use english for terminal activities

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,9 @@ if status is-interactive
     fish_config theme choose "fish default"
 
     set -gx pure_color_success green
+
+    # https://askubuntu.com/a/264286
+    set -gx LC_ALL "en_GB.UTF-8"
+    set -gx LANG "en_GB.UTF-8"
+    set -gx LANGUAGE "en_GB:en"
 end
